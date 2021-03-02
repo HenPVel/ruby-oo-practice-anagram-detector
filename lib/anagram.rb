1c9577d28f8efa9775daa5037baf061b4998196f
Henry <hvelas93@gmail.com>
2021-03-01 19:04:58 -0500

    unfinished anagram practice lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+def Anagram
+
+    attr_reader word
+
+    @@all=[]
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(array)
+        
+    end
+
+    binding.pry
+end
